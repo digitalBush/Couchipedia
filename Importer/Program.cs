06f82e1c199bd5a493b41a13be147a6041460ca8
Josh Bush <josh@digitalbush.com>
2011-03-17 18:51:21 -0500

    Skelaton project

---
+++ Importer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Importer {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
